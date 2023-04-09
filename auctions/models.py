854be67e12e4ea8ad1b200c8675ec4f3b63eea02
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-04-09 22:27:19 +0700

    Added active listings display

--- auctions/models.py
+++ auctions/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
     pass
 
@@ -13,6 +12,7 @@ class Listing(models.Model):
     starting_price = models.DecimalField(max_digits=8, decimal_places=2)
     date = models.DateTimeField(auto_now_add=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    active = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.seller} is selling {self.title} for a base price of ${self.starting_price}"
