7f75193f5ade58e0331002ec75e27dd21208467b
Richard Calvin Yong <r.calvin.yong@gmail.com>
2023-04-18 21:43:38 +0700

    Added bid status badge to watchlist page

--- auctions/templatetags/custom_filters.py
+++ auctions/templatetags/custom_filters.py
@@ -30,3 +30,33 @@ def time_ago(date_created):
             diff_str = f"{diff.seconds} seconds ago"
         
     return re.sub(r"^1 (\w+)s ago$", r"1 \1 ago", diff_str)
+
+
+# https://stackoverflow.com/questions/420703/how-do-i-add-multiple-arguments-to-my-custom-template-filter-in-a-django-templat
+@register.simple_tag
+def bid_status_badge(user, listing):
+    """ Returns a bootstrap badge indicating the user's bid status """
+
+    try: 
+        highest_bid = listing.bids.order_by('-value')[0].value
+    except IndexError:
+        return "<span class='badge text-bg-secondary'>No Bids</span>"
+    
+    user_bid = listing.bids.filter(bidder=user)
+
+    if listing.active:    
+        if user_bid:
+            if user_bid.order_by('-value')[0].value == highest_bid:
+                return "<span class='badge text-bg-success'>You have the leading bid</span>"
+            else:
+                return "<span class='badge text-bg-danger'>Place a new bid to be in the lead</span>"
+        else:
+            return "<span class='badge text-bg-secondary'>You have not placed a bid</span>"
+    else:
+        if user_bid:
+            if user_bid.order_by('-value')[0].value == highest_bid:
+                return "<span class='badge text-bg-success'>You won</span>"
+            else:
+                return "<span class='badge text-bg-danger'>You lost</span>"
+        else:
+            return "<span class='badge text-bg-secondary'>You did not placed a bid</span>"
